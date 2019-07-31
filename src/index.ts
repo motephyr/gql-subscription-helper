28ce9552984462088af18fe04fbfe1bd9cbea80f
motephyr <motephyr@gmail.com>
2019-07-31 14:44:14 +0800

    fix package name

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,45 @@
-export default function (prev: Object, obj: { mutation: Object, node: { id: Number } }): any {
-  const symbol = Object.keys(prev)[0]
-  const prevObjs = Object.values(prev)[0]
 
-  switch (obj.mutation) {
-    case "CREATED": {
-      const newAuthor = obj.node;
-      // Don't double add the author
-      if (prevObjs.some((x: { id: Number; }) => x.id === newAuthor.id)) {
-        return prev;
-      } else {
-        // Author not found, add it
+export default class GQLSubscriptionHelper {
+
+  static updateQuery(prev: Object, obj: { mutation: Object, node: { id: Number } }): any {
+    const symbol = Object.keys(prev)[0]
+    const prevObjs = Object.values(prev)[0]
+
+    switch (obj.mutation) {
+      case "CREATED": {
+        const newAuthor = obj.node;
+        // Don't double add the author
+        if (prevObjs.some((x: { id: Number; }) => x.id === newAuthor.id)) {
+          return prev;
+        } else {
+          // Author not found, add it
+          return Object.assign({}, prev, {
+            [symbol]: [newAuthor, ...prevObjs]
+          });
+        }
+      }
+      case "UPDATED": {
+        const updatedAuthor = obj.node;
+        // Replace previous author with updated one
         return Object.assign({}, prev, {
-          [symbol]: [newAuthor, ...prevObjs]
+          [symbol]: prevObjs.map((x: { id: Number; }) =>
+            x.id === updatedAuthor.id
+              ? updatedAuthor
+              : x
+          )
         });
       }
+      case "DELETED": {
+        const deletedAuthor = obj.node;
+        // Delete author
+        return Object.assign({}, prev, {
+          [symbol]: prevObjs.filter(
+            (x: { id: Number; }) => x.id !== deletedAuthor.id
+          )
+        });
+      }
+      default:
+        return prev;
     }
-    case "UPDATED": {
-      const updatedAuthor = obj.node;
-      // Replace previous author with updated one
-      return Object.assign({}, prev, {
-        [symbol]: prevObjs.map((x: { id: Number; }) =>
-          x.id === updatedAuthor.id
-            ? updatedAuthor
-            : x
-        )
-      });
-    }
-    case "DELETED": {
-      const deletedAuthor = obj.node;
-      // Delete author
-      return Object.assign({}, prev, {
-        [symbol]: prevObjs.filter(
-          (x: { id: Number; }) => x.id !== deletedAuthor.id
-        )
-      });
-    }
-    default:
-      return prev;
   }
 }
